d2bdd956017fc63209fdae4a6caa7f1d1969688b
shiva.aggarwal <shvrekhan@gmail.com>
2025-07-06 14:46:51 +0530

    rule dev 1.0 => init commit.

---
+++ src/test/java/springPractice/rule/RuleApplicationTests.java
@@ -0,0 +1,13 @@
+package springPractice.rule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
